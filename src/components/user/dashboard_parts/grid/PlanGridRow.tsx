c84e60ed384804cf99e7ade0489bf100672270b1
AntonDmitriev <44076196+AntonDmitriev@users.noreply.github.com>
2022-06-15 15:20:36 -0500

    Added accordion, hopefully will enable display of subtasks.

--- src/components/user/dashboard_parts/grid/PlanGridRow.tsx
+++ src/components/user/dashboard_parts/grid/PlanGridRow.tsx
@@ -7,12 +7,17 @@ import { useState } from 'react';
 import ProgressBar from './progress_bar/ProgressBar';
 import NotesIcon from '@mui/icons-material/Notes';
 import Box from '@mui/material/Box';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import { AccordionDetails } from '@mui/material';
 
 
 {/* <Grid
@@ -25,65 +30,94 @@ alignItems="center"> */}
 
 export default function PlanGridRow (props:any):JSX.Element {
 
-    console.log('in plan grid row');
 
     return (
         <div>
-
-            <Grid
-            container
-            direction = "row"
-            spacing = {1}>
-
                 <Grid item>
-                    <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                        <Button>One</Button>
-                        <Button>Two</Button>
-                    </ButtonGroup>
-                </Grid>
 
-                <Grid item>
-                    <TextField
-                        hiddenLabel
-                        id="filled-hidden-label-small"
-                        defaultValue="Small"
-                        variant="filled"
-                        size="small"
-                        />
-                </Grid>
-
-                <Grid item>
-                    <Button variant = "contained">06/15/2022</Button>
-                </Grid>
-
-                <Grid item>
-                        
-                    <ProgressBar/>
-                </Grid>
-
-                <Grid item>
-                    <Checkbox/>
-                </Grid>
-
-                {/* Add note option later */}
-
-                <Grid item>
-                    <ButtonGroup
+                    <Accordion>
+                        <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
                         >
-                            <Button> 
-                                <ArrowDropUpIcon/> 
-                            </Button>
-                            <Button> 
-                                <ArrowDropDownIcon/>
-                            </Button>
-                            <Button>
-                                <MoreVertIcon/>
-                            </Button>
 
-                    </ButtonGroup>
-                </Grid>
+                        <Grid
+                            container
+                            direction = "row"
+                            spacing = {3}>
+                                
+                        <Grid item>
+                            <ButtonGroup  aria-label="button group">
+                                <Button>CSE 503S</Button>
+                                <Button>6 - 15 - 22</Button>
+                            </ButtonGroup>
+                        </Grid>
 
-            </Grid>
+                        <Grid item>
+                            <TextField
+                                fullWidth
+                                hiddenLabel
+                                id="filled-hidden-label-small full-width-text-field"
+                                defaultValue=""
+                                size="small"
+                                // margin = "none"
+                                sx = {{
+                                    width: "200px",
+                                    top: "6px"
+                                }}
+                                variant = "standard"
+                                />
+                        </Grid>
+
+                        {/* <Grid item>
+                            <Button variant="outlined">06/15/2022</Button>
+                        </Grid> */}
+
+                        <Grid item>
+                            <ProgressBar/>
+                        </Grid>
+
+                        <Grid item>
+                            <Checkbox/>
+                        </Grid>
+
+                        {/* Add note option later */}
+
+                        <Grid item>
+                            <ButtonGroup
+                                >
+                                    <Button> 
+                                        <ArrowDropUpIcon/> 
+                                    </Button>
+                                    <Button> 
+                                        <ArrowDropDownIcon/>
+                                    </Button>
+                                    <Button>
+                                        <MoreVertIcon/>
+                                    </Button>
+
+                            </ButtonGroup>
+                        </Grid>
+
+
+                           
+                    </Grid>
+                        </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                        </AccordionDetails>
+                        
+                    </Accordion>
+
+                        
+
+                    </Grid>
+
+             
 
         </div>
     )
